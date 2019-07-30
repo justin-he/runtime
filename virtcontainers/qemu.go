kata-containers/kata-containers

commit 50e263d9436ec3cd5ca452630a12fa2c1661c482

qemu: support vfio pass x-pci-vendor-id and x-pci-device-id pass
since some vendor id like 1ded can not be identified by virtio-pci
driver, so need to pass a specified vendor id to qemu.

Fixes: #1894

Signed-off-by: Ace-Tang <aceapril@126.com>

virtcontainers/qemu.go
@@ -1172,7 +1172,7 @@ func (q *qemu) hotplugNetDevice(endpoint Endpoint, op operation) error {
 			return err
 		}
 		if machine.Type == QemuCCWVirtio {
-			return q.qmpMonitorCh.qmp.ExecuteNetCCWDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), addr, bridge.ID, int(q.config.NumVCPUs))
+			return q.qmpMonitorCh.qmp.ExecuteNetCCWDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), bridge.ID, int(q.config.NumVCPUs))
 		}
 		return q.qmpMonitorCh.qmp.ExecuteNetPCIDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), addr, bridge.ID, romFile, int(q.config.NumVCPUs), defaultDisableModern)
 	}

virtcontainers/device/config/config.go
@@ -167,6 +167,12 @@ type VFIODev struct {
 
 	// sysfsdev of VFIO mediated device
 	SysfsDev string
+
+	// VendorID specifies vendor id
+	VendorID string
+
+	// DeviceID specifies device id
+	DeviceID string
 }
 
 // RNGDev represents a random number generator device

virtcontainers/physical_endpoint.go
@@ -79,8 +79,16 @@ func (endpoint *PhysicalEndpoint) Attach(h hypervisor) error {
 	}
 
 	// TODO: use device manager as general device management entrance
+	var vendorID, deviceID string
+	if splits := strings.Split(endpoint.VendorDeviceID, " "); len(splits) == 2 {
+		vendorID = splits[0]
+		deviceID = splits[1]
+	}
+
 	d := config.VFIODev{
-		BDF: endpoint.BDF,
+		BDF:      endpoint.BDF,
+		VendorID: vendorID,
+		DeviceID: deviceID,
 	}
 
 	return h.addDevice(d, vfioDev)

virtcontainers/qemu_arch_base.go
@@ -551,7 +551,9 @@ func (q *qemuArchBase) appendVFIODevice(devices []govmmQemu.Device, vfioDev conf
 
 	devices = append(devices,
 		govmmQemu.VFIODevice{
-			BDF: vfioDev.BDF,
+			BDF:      vfioDev.BDF,
+			VendorID: vfioDev.VendorID,
+			DeviceID: vfioDev.DeviceID,
 		},
 	)
 

virtcontainers/qemu_arch_base_test.go
@@ -419,6 +419,28 @@ func TestQemuArchBaseAppendVFIODevice(t *testing.T) {
 	testQemuArchBaseAppend(t, vfDevice, expectedOut)
 }
 
+func TestQemuArchBaseAppendVFIODeviceWithVendorDeviceID(t *testing.T) {
+	bdf := "02:10.1"
+	vendorID := "0x1234"
+	deviceID := "0x5678"
+
+	expectedOut := []govmmQemu.Device{
+		govmmQemu.VFIODevice{
+			BDF:      bdf,
+			VendorID: vendorID,
+			DeviceID: deviceID,
+		},
+	}
+
+	vfDevice := config.VFIODev{
+		BDF:      bdf,
+		VendorID: vendorID,
+		DeviceID: deviceID,
+	}
+
+	testQemuArchBaseAppend(t, vfDevice, expectedOut)
+}
+
 func TestQemuArchBaseAppendSCSIController(t *testing.T) {
 	var devices []govmmQemu.Device
 	assert := assert.New(t)

justin-he/runtime

commit b103f4c960e97f633b7d788b0cc69cbb33044443

virtcontainers: fix missing parameter in ExecuteNetCCWDeviceAdd()
This fix the regression caused by commit 50e263d9436e ("qemu: support
vfio pass x-pci-vendor-id and x-pci-device-id pass")

Fixes: #1489
Signed-off-by: Jia He <justin.he@arm.com>

virtcontainers/qemu.go
@@ -1184,7 +1184,7 @@ func (q *qemu) hotplugNetDevice(endpoint Endpoint, op operation) error {
 			return err
 		}
 		if machine.Type == QemuCCWVirtio {
-			return q.qmpMonitorCh.qmp.ExecuteNetCCWDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), bridge.ID, int(q.config.NumVCPUs))
+			return q.qmpMonitorCh.qmp.ExecuteNetCCWDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), addr, bridge.ID, int(q.config.NumVCPUs))
 		}
 		return q.qmpMonitorCh.qmp.ExecuteNetPCIDeviceAdd(q.qmpMonitorCh.ctx, tap.Name, devID, endpoint.HardwareAddr(), addr, bridge.ID, romFile, int(q.config.NumVCPUs), defaultDisableModern)
 	}